892de91426f2b3e4e8b86b4d55209cd4332bc056
dean <deandeanpayne@gmail.com>
2023-04-24 19:40:48 -0700

    modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app.module.ts 	modified:   src/app/inital-login-page/inital-login-page.component.html 	modified:   src/app/inital-login-page/inital-login-page.component.ts 	modified:   src/index.html 	modified:   src/styles.css 	modified:   tsconfig.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InitalLoginPageComponent } from './inital-login-page/inital-login-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +16,11 @@ import { InitalLoginPageComponent } from './inital-login-page/inital-login-page.
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    NoopAnimationsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
