b0be657e34acec9026076184203b4fcfec281cd1
dean <deandeanpayne@gmail.com>
2023-04-24 19:05:04 -0700

    modified:   src/app/app-routing.module.ts 	modified:   src/app/app.component.html 	modified:   src/app/inital-login-page/inital-login-page.component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InitalLoginPageComponent } from './inital-login-page/inital-login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: InitalLoginPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
