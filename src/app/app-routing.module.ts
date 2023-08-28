a0d4ffe3a55558b8c56739e044759b7ba2cf9f99
LSierraOsorio <Luis.SierraOsorio99@gmail.com>
2023-08-28 15:46:13 -0700

    created register page, and added route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
+// Luis 08/28/23 - Created register page and worked on it
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InitalLoginPageComponent } from './inital-login-page/inital-login-page.component';
+import { CreateAccountPageComponent } from './create-account-page/create-account-page/create-account-page.component';
 
 const routes: Routes = [
-  {path: '', component: InitalLoginPageComponent},
+  {path: 'login', component: InitalLoginPageComponent},
+  {path: 'register', component: CreateAccountPageComponent},
 ];
 
 @NgModule({
