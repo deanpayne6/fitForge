9ebd38a9b94ad84770955cb1e6374664d91c1e53
LSierraOsorio <Luis.SierraOsorio99@gmail.com>
2023-08-30 16:00:48 -0700

    Added more validators
    added more validators to the text fields, made the register button only clickable if all fields are valid

--- src/app/create-account-page/create-account-page/create-account-page.component.ts
+++ src/app/create-account-page/create-account-page/create-account-page.component.ts
@@ -10,6 +10,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class CreateAccountPageComponent {
 
   userRegisterForm: FormGroup;
+  showPasswordRequirements = false;
 
   constructor(private fb: FormBuilder){};
 
@@ -18,14 +19,45 @@ export class CreateAccountPageComponent {
       userEmail: ['', [Validators.required, Validators.email]],
       userFirstName: ['', [Validators.required]],
       userLastName: ['', [Validators.required]],
-      userAge: ['',[Validators.required]],
+      userAge: ['',[Validators.required, Validators.min(18)]],
       username: ['',[Validators.required]],
       userPassword: ['',[Validators.required]],
       userPasswordConfirm: ['',[Validators.required]],
+      termsAndConditions: ['', [Validators.requiredTrue]],
+      termsCheckboxStatus: [false],
     })
+  }
+  
+  handleCheckboxChange() {
+    this.userRegisterForm.patchValue({
+      termsCheckboxStatus: !this.userRegisterForm.get('termsCheckboxStatus').value
+    });
+  }
+
+  isPasswordValid(){
+    const password = this.userRegisterForm.get('userPassword').value;
+
+    const isLengthValid = password.length >= 8;
+    const hasSpecialCharacter = this.specialCharacterPattern.test(password);
+    const hasNumber = this.numberPattern.test(password);
+
+    return isLengthValid && hasSpecialCharacter && hasNumber;
+  }
+
+  isConfirmPasswordValid() {
+    const password = this.userRegisterForm.get('userPassword').value;
+    const confirmPassword = this.userRegisterForm.get('userPasswordConfirm').value;
+    
+    if (password === ""){
+      return false;
+    }
+    return password === confirmPassword;
   }
 
   register(){
     
   }
+
+  specialCharacterPattern = /[!@#$%^&*(),.?":{}|<>]/;
+  numberPattern = /[0-9]/;
 }
