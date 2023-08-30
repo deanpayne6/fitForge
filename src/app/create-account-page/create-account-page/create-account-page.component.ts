be61f78db402e1a3f45ad135a03ea044296f4d89
LSierraOsorio <Luis.SierraOsorio99@gmail.com>
2023-08-30 10:32:52 -0700

    Added bootstrap, created registration, added some validators
    I was able to implement bootstrap, added the text fields for the required information. Added some validators to the textboxes.

--- src/app/create-account-page/create-account-page/create-account-page.component.ts
+++ src/app/create-account-page/create-account-page/create-account-page.component.ts
@@ -6,11 +6,24 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   templateUrl: './create-account-page.component.html',
   styleUrls: ['./create-account-page.component.css']
 })
+
 export class CreateAccountPageComponent {
 
-  userRegisterForm: FormGroup
+  userRegisterForm: FormGroup;
 
   constructor(private fb: FormBuilder){};
+
+  ngOnInit(): void {
+    this.userRegisterForm = this.fb.group({
+      userEmail: ['', [Validators.required, Validators.email]],
+      userFirstName: ['', [Validators.required]],
+      userLastName: ['', [Validators.required]],
+      userAge: ['',[Validators.required]],
+      username: ['',[Validators.required]],
+      userPassword: ['',[Validators.required]],
+      userPasswordConfirm: ['',[Validators.required]],
+    })
+  }
 
   register(){
     
