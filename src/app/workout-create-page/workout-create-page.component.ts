c3bf60e70a788287e93ea87ab3c25ea1dfd5ded9
LSierraOsorio <Luis.SierraOsorio99@gmail.com>
2023-09-24 08:34:27 -0700

    Adding individual workouts

--- src/app/workout-create-page/workout-create-page.component.ts
+++ src/app/workout-create-page/workout-create-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, Renderer2, ElementRef } from '@angular/core';
+import { WorkoutService } from './workout.service'
 
 
 @Component({
@@ -12,6 +13,17 @@ export class WorkoutCreatePageComponent {
   workoutOptions: string[] = ['Option 1', 'Option 2', 'Option 3'];
   muscleGroupContainers: { option: string, individualWorkouts: any[] }[] = [];
   selectedOption: string = '';
+  dropdownOptions: string[] = [];
+
+  constructor(private workoutService: WorkoutService) {}
+
+  ngOnInit() {
+    this.workoutService.getDropdownOptions().subscribe((options) => {
+      this.dropdownOptions = options;
+    });
+  }
+
+
 
   createMuscleGroupContainer() {
     this.muscleGroupContainers.push({ option: this.selectedOption, individualWorkouts: [] });
--- src/app/workout-create-page/workout.service.ts
+++ src/app/workout-create-page/workout.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 
 @Injectable({
@@ -6,6 +8,9 @@ import { Injectable } from '@angular/core';
 })
 export class WorkoutService {
 
+  constructor(private http: HttpClient) {}
+  getDropdownOptions(): Observable<string[]> {
+    return this.http.get<string[]>('https://api.example.com/options');
+  }
 
-  constructor() { }
 }
