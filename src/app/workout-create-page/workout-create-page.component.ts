99d885ea18e1a35b2802cb32011bea401a477ae6
LSierraOsorio <Luis.SierraOsorio99@gmail.com>
2023-10-19 13:13:16 -0700

    dynamically show workuts, refactored code

--- src/app/workout-create-page/workout-create-page.component.ts
+++ src/app/workout-create-page/workout-create-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, Renderer2, ElementRef } from '@angular/core';
 import { WorkoutService } from './workout.service'
+import { UserService } from '../user.service';
 
 
 @Component({
@@ -10,97 +11,62 @@ import { WorkoutService } from './workout.service'
 
 
 export class WorkoutCreatePageComponent {
-  workoutOptions: string[] = ['Bicep','Tricep','Chest','Back','Shoulder','Abs','Legs'];
-  muscleGroupContainers: { option: string, individualWorkouts: any[] }[] = [];
-  selectedOption: string = '';
-  dropdownOptions: string[] = [];
-  selectedIndividualWorkoutOption: string = ""
-  individualExerciseOptionsString: string = ""
-
-  numOfSets: string = '';
-  numOfReps: string = '';
-  restTime: string = '';
-  weight: string = '';
-  target: string = '';
-  vidLink: string = '';
-
-  selectedOption1: string = '';
-  selectedOption2: string = '';
-  selectedOption3: string = '';
-  selectedWorkoutLength: string = 'short';
-  selectedWorkoutArray: string[] = [];
-
-  numOfSetsArray: string[] = [];
-  numOfRepsArray: string[] = [];
-  restTimeArray: string[] = [];
-  weightArray: string[] = [];
-  targetArray: string[] = [];
-  vidLinkArray: string[] = [];
-  individualExerciseOptionsStringArray: string[] = [];
-
-
-  constructor(private workoutService: WorkoutService) {}
-
+  constructor(private workoutService: WorkoutService, public userService: UserService) {};
   ngOnInit() {}
+  // Variables
+  workoutOptions: string[] = ['Chest','Shoulders','Back','Biceps','Triceps','Abs','Legs'];
+  workoutLengthOptions: string[] = ['short', 'medium', 'long'];
+  selectedWorkoutLength: string = 'short';
+  selectedOption1: string = "Chest";
+  selectedOption2: string = "";
+  selectedOption3: string = "";
+  selectedOptionArray: string[] = [];
 
-  generateWorkout() {
+  individualMuscleContainer: any[] = [];
 
-    if (this.selectedOption1 !== ''){
-      this.selectedWorkoutArray.push(this.selectedOption1)
+  workoutName: string = '';
+  workoutSets: string = '';
+  workoutReps: string = '';
+  workoutRest: string = '';
+  workoutTarget: string = '';
+  workoutLink: string = '';
+
+  generateButtonClicked = false;
+
+  validateSelection(){
+    this.selectedOptionArray = [];
+    this.selectedOptionArray.push(this.selectedOption1);
+
+    if (this.selectedOption2 !== ""){
+      this.selectedOptionArray.push(this.selectedOption2);
     }
-    if (this.selectedOption2 !== ''){
-      this.selectedWorkoutArray.push(this.selectedOption2)
-    }
-    if (this.selectedWorkoutLength !== 'short'){
-      if (this.selectedOption3 !== ''){
-        this.selectedWorkoutArray.push(this.selectedOption3);
+
+    if (this.selectedWorkoutLength !== "short"){
+      if (this.selectedOption3 !== ""){
+        this.selectedOptionArray.push(this.selectedOption3)
       }
     }
+  }
 
-    this.workoutService.getGenerateWorkout(this.selectedWorkoutArray, this.selectedWorkoutLength, 'andrew').subscribe(response => {
-      console.log(response);
-  
-      this.createMuscleGroupContainer();
-  
+  generateWorkout(){
+    this.validateSelection();
+
+    this.workoutService.getGenerateWorkout(this.selectedOptionArray, this.selectedWorkoutLength, 'andrew').subscribe(response => {
       response.forEach((data, index) => {
-        this.addExercise(this.muscleGroupContainers[0], index);
-        const i = index * 8;
-        this.individualExerciseOptionsStringArray[i] = data[1];
-        this.numOfSetsArray[i] = data[2];
-        this.numOfRepsArray[i] = data[3];
-        this.restTimeArray[i] = data[4];
-        this.weightArray[i] = data[5];
-        this.targetArray[i] = data[6];
-        this.vidLinkArray[i] = data[7];
-      });
-    }, error => {
+        this.individualMuscleContainer.push([
+          this.workoutName = data[1],
+          this.workoutSets = data[2],
+          this.workoutReps = data[3],
+          this.workoutRest = data[4],
+          this.workoutTarget = data[5],
+          this.workoutLink = data[6],
+        ]);
+      })
+      this.generateButtonClicked = true;
+      console.log(this.userService.getUser());
+      }, error => {
       console.error(error);
     });
   }
 
-  createMuscleGroupContainer() {
-    this.muscleGroupContainers.push({ option: this.selectedOption, individualWorkouts: [] });
-  }
-
-  addExercise(mainContainer: any, index: number) {
-    // const index = this.muscleGroupContainers.findIndex(container => container === mainContainer);
-    mainContainer.individualWorkouts.push({});
-    this.individualExerciseOptionsStringArray[index] = '';
-    this.numOfSetsArray[index] = '';
-    this.numOfRepsArray[index] = '';
-    this.restTimeArray[index] = '';
-    this.weightArray[index] = '';
-    this.targetArray[index] = '';
-    this.vidLinkArray[index] = '';
-  }
-
-  removeExercise(mainContainer: any, index: number) {
-    mainContainer.individualWorkouts.splice(index, 1);
-    this.numOfSetsArray.splice(index, 1);
-    this.numOfRepsArray.splice(index, 1);
-    this.restTimeArray.splice(index, 1);
-    this.weightArray.splice(index, 1);
-    this.targetArray.splice(index, 1);
-    this.vidLinkArray.splice(index, 1);
-  }
 }
