c943ddbdc66f94b91564e9f02fb572e0c420cbea
LSierraOsorio <Luis.SierraOsorio99@gmail.com>
2023-10-21 16:40:46 -0700

    added update workout
    - user can now update workout
    - added regenerate and accept button
        - accept - not implement
        - regenerate - will allow the user to regenerate daily workout

--- src/app/workout-create-page/workout-create-page.component.ts
+++ src/app/workout-create-page/workout-create-page.component.ts
@@ -30,8 +30,10 @@ export class WorkoutCreatePageComponent {
 
   //this is an array that keeps track of the individual workout divs, allows for easy access of the divs
   individualMuscleContainer: any[] = [];
+  emptyList:boolean = true;
 
   // variables used for the creation of the object inside of each individualMuscleContainter
+  workoutMuscleGroup:string = '';
   workoutName: string = '';
   workoutSets: string = '';
   workoutReps: string = '';
@@ -42,12 +44,17 @@ export class WorkoutCreatePageComponent {
   // these options are for the when the edit button is clicked
   editDivVisible: boolean = false;
   workoutNameDisplay: string = '';
+
+  // options for selecting new workout
+  newWorkoutDivVisible: boolean = false;
+  newSelectedWorkoutName: string = "";
+  newWorkoutsReturnedArray: string[] = [];
   
   // this is to remove the generate button after it was has been clicked.
   generateButtonClicked = false;
 
   // this is being used to keep track of the index of the selected edited div
-  selectedIndex:number;
+  selectedDivIndex:number;
 
   // this function is meant to valdiate the user options, ensures that no empty strings are passed to the backend
   validateSelection(){
@@ -69,11 +76,12 @@ export class WorkoutCreatePageComponent {
   generateWorkout(){
     // calling the validating function
     this.validateSelection();
-
     // calling the workout service to make a call to the backend, pushed the data onto the individualMuscleContainer div, with neccesary info
     this.workoutService.getGenerateWorkout(this.selectedOptionArray, this.selectedWorkoutLength, 'andrew').subscribe(response => {
+      console.log(response);
       response.forEach((data, index) => {
         this.individualMuscleContainer.push([
+          this.workoutMuscleGroup = data[0],
           this.workoutName = data[1],
           this.workoutSets = data[2],
           this.workoutReps = data[3],
@@ -94,19 +102,81 @@ export class WorkoutCreatePageComponent {
   // function happens when the edit button is clicked, Shows selected workout, blurs out the background, and saves the selected index
   showEditDiv(index: number) {
     this.editDivVisible = true;
-    this.workoutNameDisplay = this.individualMuscleContainer[index][0];
-    this.selectedIndex = index;
+    this.workoutNameDisplay = this.individualMuscleContainer[index][1];
+    this.selectedDivIndex = index;
   }
 
   // just cancles the edit page
   cancelEditWorkout() {
       this.editDivVisible = false;
+      this.newWorkoutDivVisible = false;
+      this.newWorkoutsReturnedArray = [];
   }
 
-  // deletes the div that was selected
+  // deletes the div that was selected, also resets the generate workout incase all workouts are deleted
   deleteWorkout(){
-    this.individualMuscleContainer.splice(this.selectedIndex, 1);
+    this.individualMuscleContainer.splice(this.selectedDivIndex, 1);
     this.editDivVisible = false;
+
+    if (this.individualMuscleContainer.length === 0){
+      this.generateButtonClicked = false;
+      this.selectedOption1 = "Chest";
+      this.selectedOption2 = "";
+      this.selectedOption3 = "";
+      this.selectedWorkoutLength = 'short';
+    }
   }
 
+  getNewWorkouts(){
+    this.editDivVisible = false;
+    this.newWorkoutDivVisible = true;
+
+    console.log(this.workoutNameDisplay)
+    this.workoutService.getNewWorkoutNames(this.workoutNameDisplay, 'andrew').subscribe(response => {
+
+      console.log(response);
+      this.newWorkoutsReturnedArray = response.slice();
+      this.newSelectedWorkoutName = this.newWorkoutsReturnedArray[0];
+      }, error => {
+      console.error(error);
+    });
+  }
+
+  confirmNewWorkout(){
+    this.workoutService.getUpdatedWorkout(this.individualMuscleContainer, this.newSelectedWorkoutName, this.selectedDivIndex, 'andrew').subscribe(response => {
+      this.individualMuscleContainer = [];
+      console.log(response);
+      response.forEach((data, index) => {
+        this.individualMuscleContainer.push([
+          this.workoutMuscleGroup = data[0],
+          this.workoutName = data[1],
+          this.workoutSets = data[2],
+          this.workoutReps = data[3],
+          this.workoutRest = data[4],
+          this.workoutTarget = data[5],
+          this.workoutLink = data[6],
+        ]);
+      })
+      // this makes the generated button disappear when clicked
+      this.generateButtonClicked = true;
+      this.cancelEditWorkout()
+      console.log(this.userService.getUser());
+      }, error => {
+      console.error(error);
+    });
+  }
+
+  acceptWorkout(){
+    console.log("Accept workout button clicked")
+  }
+
+  regenerateWorkout(){
+    console.log("regenerate workout button clicked")
+    this.generateButtonClicked = false;
+    this.individualMuscleContainer =[];
+    this.selectedOption1 = "Chest";
+    this.selectedOption2 = "";
+    this.selectedOption3 = "";
+    this.selectedWorkoutLength = 'short';
+  }
 }
--- src/app/workout-create-page/workout.service.ts
+++ src/app/workout-create-page/workout.service.ts
@@ -20,4 +20,28 @@ export class WorkoutService {
 
     return this.http.post<{ data: any }>(`${this.apiUrl}/generateWorkout`, body);
   }
+
+  // http://localhost:3200/sendMuscleSwap
+
+
+  getNewWorkoutNames(workoutName: string, username: string): Observable<any>{
+    const body = {
+      workoutName,
+      username,
+    };
+
+    return this.http.post<{data: any}>(`${this.apiUrl}/sendMuscleSwap`, body);
+  }
+
+  // updateWorkout
+  getUpdatedWorkout(workoutList: any, newWorkout: string, index: number, username: string): Observable <any>{
+    const body = {
+      workoutList,
+      newWorkout,
+      index,
+      username
+    }
+
+    return this.http.post<{data: any}>(`${this.apiUrl}/updateWorkout`, body);
+  }
 }
