41e9dbe2e92df06022dfe127cb7401ad3e166254
LSierraOsorio <Luis.SierraOsorio99@gmail.com>
2023-10-21 21:55:17 -0700

    refactored ts code and added comments

--- src/app/workout-create-page/workout-create-page.component.ts
+++ src/app/workout-create-page/workout-create-page.component.ts
@@ -12,7 +12,7 @@ import { UserService } from '../user.service';
 
 export class WorkoutCreatePageComponent {
   constructor(private workoutService: WorkoutService, public userService: UserService) {};
-  ngOnInit() {}
+  ngOnInit() {};
 
   // display options for workouts, lengths
   workoutOptions: string[] = ['Chest','Shoulders','Back','Biceps','Triceps','Abs','Legs'];
@@ -29,16 +29,6 @@ export class WorkoutCreatePageComponent {
 
   //this is an array that keeps track of the individual workout divs, allows for easy access of the divs
   individualMuscleContainer: any[] = [];
-  emptyList:boolean = true;
-
-  // variables used for the creation of the object inside of each individualMuscleContainter
-  workoutMuscleGroup:string = '';
-  workoutName: string = '';
-  workoutSets: string = '';
-  workoutReps: string = '';
-  workoutRest: string = '';
-  workoutTarget: string = '';
-  workoutLink: string = '';
 
   // these options are for the when the edit button is clicked
   editDivVisible: boolean = false;
@@ -55,7 +45,9 @@ export class WorkoutCreatePageComponent {
   // this is being used to keep track of the index of the selected edited div
   selectedDivIndex:number;
 
-  // this function is meant to valdiate the user options, ensures that no empty strings are passed to the backend
+
+  // ----------> HELPER FUNCTIONS <----------
+  // this function is meant to see what options to push onto the backend for workouts
   validateSelection(){
     this.selectedOptionArray = [];
     this.selectedOptionArray.push(this.selectedOption1);
@@ -66,57 +58,27 @@ export class WorkoutCreatePageComponent {
 
     if (this.selectedWorkoutLength !== "short"){
       if (this.selectedOption3 !== ""){
-        this.selectedOptionArray.push(this.selectedOption3)
+        this.selectedOptionArray.push(this.selectedOption3);
       }
     }
-  }
+  };
 
-  // this is the function actually generating the workouts
-  generateWorkout(){
-    // calling the validating function
-    this.validateSelection();
-    // calling the workout service to make a call to the backend, pushed the data onto the individualMuscleContainer div, with neccesary info
-    this.workoutService.getGenerateWorkout(this.selectedOptionArray, this.selectedWorkoutLength, 'andrew').subscribe(response => {
-      console.log(response);
-      response.forEach((data, index) => {
-        this.individualMuscleContainer.push([
-          this.workoutMuscleGroup = data[0],
-          this.workoutName = data[1],
-          this.workoutSets = data[2],
-          this.workoutReps = data[3],
-          this.workoutRest = data[4],
-          this.workoutTarget = data[5],
-          this.workoutLink = data[6],
-        ]);
-      })
-      // this makes the generated button disappear when clicked
-      this.generateButtonClicked = true;
-      // THIS LOG DOES NOT WORK!!!! - ALEX FIX
-      console.log(this.userService.getUser());
-      }, error => {
-      console.error(error);
-    });
-  }
+  // creating workout array with the use of an object
+  createWorkouts(data){
+    const object = {
+      workoutMuscleGroup: data[0],
+      workoutName: data[1],
+      workoutSets: data[2],
+      workoutReps: data[3],
+      workoutRest: data[4],
+      workoutTarget: data[5],
+      workoutLink: data[6],
+    };
+    this.individualMuscleContainer.push(object);
+  };
 
-  // function happens when the edit button is clicked, Shows selected workout, blurs out the background, and saves the selected index
-  showEditDiv(index: number) {
-    this.editDivVisible = true;
-    this.workoutNameDisplay = this.individualMuscleContainer[index][1];
-    this.selectedDivIndex = index;
-  }
-
-  // just cancles the edit page
-  cancelEditWorkout() {
-      this.editDivVisible = false;
-      this.newWorkoutDivVisible = false;
-      this.newWorkoutsReturnedArray = [];
-  }
-
-  // deletes the div that was selected, also resets the generate workout incase all workouts are deleted
-  deleteWorkout(){
-    this.individualMuscleContainer.splice(this.selectedDivIndex, 1);
-    this.editDivVisible = false;
-
+  // checking if workouts are empty
+  isWorkoutListEmpty(){
     if (this.individualMuscleContainer.length === 0){
       this.generateButtonClicked = false;
       this.selectedOption1 = "Chest";
@@ -124,58 +86,87 @@ export class WorkoutCreatePageComponent {
       this.selectedOption3 = "";
       this.selectedWorkoutLength = 'short';
     }
-  }
+  };
 
+  // ----------> MAIN FUNCTIONS <----------
+  // this is the function actually generating the workouts
+  generateWorkout(){
+    console.log("generate workout button clicked!");
+    // calling the validating function
+    this.validateSelection();
+    // calling the workout service to make a call to the backend, pushed the data onto the individualMuscleContainer div, with neccesary info
+    this.workoutService.getGenerateWorkout(this.selectedOptionArray, this.selectedWorkoutLength, 'andrew').subscribe(response => {
+      response.forEach((data, index) => {
+        this.createWorkouts(data);
+      })
+      this.generateButtonClicked = true;
+      }, error => {
+      console.error(error);
+    });
+  };
+
+  // function is meant for the edit button, shows editing div, sets the display name, and saves the index for use across entire file
+  showEditDiv(index: number) {
+    console.log("edit button clicked!");
+    this.editDivVisible = true;
+    this.workoutNameDisplay = this.individualMuscleContainer[index].workoutName;
+    this.selectedDivIndex = index;
+  };
+
+  // cancel button, redisplays the workout on page
+  cancelEditWorkout() {
+    console.log("cancel button clicked!");
+      this.editDivVisible = false;
+      this.newWorkoutDivVisible = false;
+      this.newWorkoutsReturnedArray = [];
+  };
+
+  // deletes the div that was selected, also resets the generate workout incase all workouts are deleted
+  deleteWorkout(){
+    console.log("delete button clicked!")
+    this.individualMuscleContainer.splice(this.selectedDivIndex, 1);
+    this.editDivVisible = false;
+
+    this.isWorkoutListEmpty();
+  };
+
+  // function meant to get new workouts when the swap button is clicked, makes call to backend
   getNewWorkouts(){
+    console.log("swap button clicked!")
     this.editDivVisible = false;
     this.newWorkoutDivVisible = true;
 
-    console.log(this.workoutNameDisplay)
     this.workoutService.getNewWorkoutNames(this.workoutNameDisplay, 'andrew').subscribe(response => {
-
-      console.log(response);
       this.newWorkoutsReturnedArray = response.slice();
       this.newSelectedWorkoutName = this.newWorkoutsReturnedArray[0];
       }, error => {
       console.error(error);
     });
-  }
+  };
 
+  // function for the confirming of swapping workout, sends new workoutList to the end backend
   confirmNewWorkout(){
     this.workoutService.getUpdatedWorkout(this.individualMuscleContainer, this.newSelectedWorkoutName, this.selectedDivIndex, 'andrew').subscribe(response => {
       this.individualMuscleContainer = [];
-      console.log(response);
       response.forEach((data, index) => {
-        this.individualMuscleContainer.push([
-          this.workoutMuscleGroup = data[0],
-          this.workoutName = data[1],
-          this.workoutSets = data[2],
-          this.workoutReps = data[3],
-          this.workoutRest = data[4],
-          this.workoutTarget = data[5],
-          this.workoutLink = data[6],
-        ]);
+        this.createWorkouts(data);
       })
-      // this makes the generated button disappear when clicked
-      this.generateButtonClicked = true;
-      this.cancelEditWorkout()
-      console.log(this.userService.getUser());
+      this.cancelEditWorkout();
       }, error => {
       console.error(error);
     });
-  }
+  };
 
+  // function for accepting workout, going to send info to the backend
   acceptWorkout(){
-    console.log("Accept workout button clicked")
-  }
+    console.log("Accept workout button clicked!");
+  };
 
+  // function for the regenerate button
   regenerateWorkout(){
-    console.log("regenerate workout button clicked")
+    console.log("regenerate workout button clicked!")
     this.generateButtonClicked = false;
     this.individualMuscleContainer =[];
-    this.selectedOption1 = "Chest";
-    this.selectedOption2 = "";
-    this.selectedOption3 = "";
-    this.selectedWorkoutLength = 'short';
-  }
+    this.isWorkoutListEmpty();
+  };
 }
