252212f31b99dc934dd509cfcd22f8feb282e460
LSierraOsorio <Luis.SierraOsorio99@gmail.com>
2023-11-24 22:48:40 -0800

    displaying message if daily workout is done

--- src/app/start-daily-workout/start-daily-workout.component.ts
+++ src/app/start-daily-workout/start-daily-workout.component.ts
@@ -9,10 +9,35 @@ import { StartDailyService } from './start-daily.service';
   styleUrls: ['./start-daily-workout.component.css']
 })
 export class StartDailyWorkoutComponent {
-  constructor(private dailyServive: StartDailyService, public userService: UserService, private router: Router) {};
-
-  onInit(){
+  constructor(private dailyService: StartDailyService, public userService: UserService, private router: Router) {};
+  username: string = this.userService.getUser().username;
+  
+  workoutStatusNumber: number;
+  displayDailyWorkout: boolean;
+  message:string = "Hey there big fella, you have already completed your daily workout, rest until tomorrow";
+  
+  ngOnInit() {
+    this.dailyService.getDailyWorkoutStatus(this.username).subscribe(
+      response => {
+        console.log(response);
+        this.workoutStatusNumber = response;
+        this.workoutStatusNumber = -1;
+        if (this.workoutStatusNumber === -1){
+          this.displayDailyWorkout = false;
+        }else if (this.workoutStatusNumber === 0) {
+          this.router.navigate(['/createWorkout']);
+        }else if (this.workoutStatusNumber === 1) {
+          this.displayDailyWorkout = true;
+        }
+      },
+      error => {
+        console.error(error);
+      }
+      );
+    }
     
+  displayNum(){
+    console.log(this.workoutStatusNumber);
   }
 
 }
